02d933a8f8811459b70a35cebdab9040beb4ef88
Mr Mekis <maxell.nathanael@gmail.com>
2026-08-04 11:34:43 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>Nusantara Digital - Website Scaffolding</h1>
+    </div>
+  )
+}
+
+export default App
